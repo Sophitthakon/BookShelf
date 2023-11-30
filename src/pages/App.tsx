d7fe8271e8a174fa605503b568b2a19e07957143
Sophitthakon <sptkmm06@gmail.com>
2023-11-30 19:42:17 +0700

    create mock data

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,24 @@
-import { useBookStore } from "../store/bookStore";
+import BookCard from "../components/BookCard";
+//import { useBookStore } from "../store/bookStore";
 
 function App() {
-  const { count, inc } = useBookStore();
+  //const { count, inc } = useBookStore();
   return (
     <>
-      <p className="text-red-800">{count}</p>
-      <button onClick={inc}> increase </button>
+      <div>
+        <h1>รายการที่คั่นไว้</h1>
+      </div>
+
+      <div>
+        <BookCard />
+      </div>
     </>
   );
 }
 
 export default App;
+
+/* 
+<p className="text-red-800">{count}</p>
+      <button onClick={inc}> increase </button>
+      */
